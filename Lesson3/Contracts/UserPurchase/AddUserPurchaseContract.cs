3b608fe76c7de667ba76465a07b03084c4a292bd
USER-PC\Alex <sanechek2011@bk.ru>
2023-06-05 18:26:59 +0300

    no message

--- Lesson3/Contracts/UserPurchase/AddUserPurchaseContract.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lesson3.Contracts.UserPurchase
-{
-    public class AddUserPurchaseContract
-    {
-        public int UserId { get; set; }
-        public int ProductId { get; set; }
-        public decimal Price { get; set; }
-    }
-}
